5fad4f5a5fdca82b73235b200dee5dac649a968b
Swamiom3 <106953535+Swamiom3@users.noreply.github.com>
2022-07-01 14:01:05 +0530

    Add files via upload

---
+++ Pattern4.java
@@ -0,0 +1,43 @@
+class Pattern4
+  {
+    public static void main(String[]args)
+
+    {
+
+     int i,j;
+
+
+       for(i=5; i>=1; i--)
+
+         {
+
+                    for(j=1; j<=i; j++) 
+                      {
+
+                     System.out.print("  " +j);
+
+                      }
+
+            System.out.println();
+      
+
+         } 
+
+           for(i=1; i<=5; i++)
+
+         {
+
+           for(j=1; j<=i; j++)
+           {
+
+           System.out.print("  " +j);
+           }
+
+
+           System.out.println();
+
+        }
+
+   }
+
+ }
